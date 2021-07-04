384d8e9f2c16303e52be0e0a6428f36ec4210db4
codehunter <hunterx6323@gmail.com>
2021-07-04 10:43:21 +0700

    delay sort with RxJs

--- src/presenters/chart.presenter.ts
+++ src/presenters/chart.presenter.ts
@@ -1,3 +1,5 @@
+import { from, Observable, of } from 'rxjs';
+import { concatMap, delay } from 'rxjs/operators';
 import { Chart, ChartItem } from '../models/chart.model';
 import { IChartView } from '../views/chart.view';
 
@@ -13,22 +15,19 @@ export class ChartPresenter implements IChartPresenter {
   sort(chart: Chart): void {
     console.log('sort');
 
-    const n = chart.itemList.length;
-    let arr = chart.itemList;
-    let i, j;
-    for (i = 0; i < n - 1; i++) {
-      for (j = 0; j < n - i - 1; j++) {
-        if (arr[j].size > arr[j + 1].size) {
-          this.swap(arr, j, j + 1);
-          console.log(arr);
+    // this.bubleSort(chart)
+    //   .pipe(delay(1000))
+    //   .subscribe((chart) => {
+    //     if (chart) this.chartView.renderChart(chart);
+    //   });
 
-          this.chartView.renderChart(chart);
-          console.log(chart);
-          return;
-        }
-      }
-    }
-    this.chartView.renderChart(chart);
+    this.bubleSortAutomate(chart)
+      .pipe(
+        concatMap(x => of(x).pipe(delay(1000)))
+      )
+      .subscribe((chart) => {
+        if (chart) this.chartView.renderChart(chart);
+      });
   }
 
   swap(arr: ChartItem[], xp: number, yp: number) {
@@ -36,4 +35,38 @@ export class ChartPresenter implements IChartPresenter {
     arr[xp] = arr[yp];
     arr[yp] = temp;
   }
+
+  private bubleSort(chart: Chart): Observable<Chart | null> {
+    const n = chart.itemList.length;
+    let arr = chart.itemList;
+    let i, j;
+    for (i = 0; i < n - 1; i++) {
+      for (j = 0; j < n - i - 1; j++) {
+        if (arr[j].size > arr[j + 1].size) {
+          this.swap(arr, j, j + 1);
+          return of(chart);
+        }
+      }
+    }
+    return of(null);
+  }
+
+  private bubleSortAutomate(chart: Chart): Observable<Chart | null> {
+    let chartStep: Chart[] = [];
+    const n = chart.itemList.length;
+    let arr = chart.itemList;
+    let i, j;
+    for (i = 0; i < n - 1; i++) {
+      for (j = 0; j < n - i - 1; j++) {
+        if (arr[j].size > arr[j + 1].size) {
+          this.swap(arr, j, j + 1);
+          // clone object
+          const currentChart:Chart = {itemList: [...chart.itemList]}
+          chartStep.push(currentChart);
+        }
+      }
+    }
+    return from(chartStep);
+  }
+
 }
